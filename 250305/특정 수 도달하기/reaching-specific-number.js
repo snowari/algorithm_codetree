bdc84b0a6e98e5c12a2943338655750c33ff1e36
snowari <70631889+snowari@users.noreply.github.com>
2025-03-05 16:23:43 +0900

    Title: 특정 수 도달하기, Time: 181ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250305/특정 수 도달하기/reaching-specific-number.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+
+let arr = fs.readFileSync(0).toString().split(" ")
+
+
+
+let sum = 0
+let cnt = 0
+for(let i=0; i<arr.length; i++){
+    if(Number(arr[i])>=250){
+        break;
+    }
+    sum +=Number(arr[i])
+    cnt ++;
+}
+
+let answer = (sum/cnt).toFixed(1)
+
+
+console.log(sum,answer)
