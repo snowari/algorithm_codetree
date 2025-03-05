7887e47d2ea3fa06bda5e5e9134777b5b91f397f
snowari <70631889+snowari@users.noreply.github.com>
2025-03-05 17:38:04 +0900

    Title: 특정 수 도달하기, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

--- 250305/특정 수 도달하기/reaching-specific-number.js
+++ 250305/특정 수 도달하기/reaching-specific-number.js
@@ -7,7 +7,13 @@ let arr = fs.readFileSync(0).toString().split(" ")
 let sum = 0
 let cnt = 0
 for(let i=0; i<arr.length; i++){
-    if(Number(arr[i])>=250 || i===9){
+    if(Number(arr[i])>=250){
+        break;
+        
+    }
+    if(i===9){
+        sum +=Number(arr[i])
+        cnt++;
         break;
     }
     sum +=Number(arr[i])
