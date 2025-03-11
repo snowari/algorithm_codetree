84fcd70316e9ad4bf85eed979e133095bf3c3c90
snowari <70631889+snowari@users.noreply.github.com>
2025-03-11 22:09:24 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 179ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250311/특정 조건에 맞게 출력하기/print-in-specific-conditions.js
+++ 250311/특정 조건에 맞게 출력하기/print-in-specific-conditions.js
@@ -2,12 +2,17 @@ const fs = require('fs')
 let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 
+
+let answer = []
+
 for (let elem of input){
-    if(elem ===0){
-        input.pop();
+    if(elem != 0){
+        answer.push(elem)
     }
 }
 
-let result = input.map(num => num %2===0? num/2: num+3 ).join(' ')
+//console.log(answer)
+
+let result = answer.map(num => num %2===0? num/2: num+3 ).join(' ')
 
 console.log(result)
