8529c8f100474a56df485e91a705c377cc127fb4
snowari <70631889+snowari@users.noreply.github.com>
2025-03-13 22:25:20 +0900

    Title: 특정 위치의 문자, Time: 187ms, Memory: 15MB, Status: Passed - CodeTree

--- 250313/특정 위치의 문자/char-in-specific-location.js
+++ 250313/특정 위치의 문자/char-in-specific-location.js
@@ -1,19 +1,16 @@
 const fs = require('fs')
 
-let input = fs.readFileSync(0).toString().trim().split(' ')
+let input = fs.readFileSync(0).toString().trim()
 
 let arr = ['L','E','B','R','O','S']
 
-let answer=0
-let index=0
+let index=-1
 
-for(let i=0; i<5; i++){
-    if(arr[i]==input){
-
+for(let i=0; i<6; i++){
+    if(arr[i]===input){
+        
         index = i  //배열 인덱스
-        answer++; // 같은 숫자 개수
-    
     }
 }
 
-answer != 0 ? console.log(index) : console.log("None")
+index != -1 ? console.log(index) : console.log("None")
