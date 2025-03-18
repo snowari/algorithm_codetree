911d8576f72ba2b8fbcf37a9daa510c679916481
snowari <70631889+snowari@users.noreply.github.com>
2025-03-18 23:45:30 +0900

    Title: 두 개의 격자 비교하기, Time: 169ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250318/두 개의 격자 비교하기/compare-two-grid.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+let input = fs.readFileSync(0).toString().trim().split('\n')
+
+let nm = input[0].split(' ').map(Number)
+const n = nm[0]
+const m = nm[1]
+
+input = input.slice(1)
+let arr1 = input.slice(0, n)
+let arr2 = input.slice(n)
+
+arr1 = arr1.map(row=> row.split(' ').map(Number))
+arr2 = arr2.map(row=> row.split(' ').map(Number))
+
+let answer  = Array(n).fill(0).map(()=> Array(m).fill(0))
+
+for(let i=0; i<n; i++){
+    for(let j=0; j<n; j++){
+        if(arr1[i][j]===arr2[i][j])
+            answer[i][j]=0
+        else
+            answer[i][j]=1
+    }
+}
+
+
+for(let row of answer){
+    str = '';
+    for(let elem of row){
+        str = str + elem + " ";
+    }
+    console.log(str)
+}
