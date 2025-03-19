91cca0c8e149f39af52abaada6fdb7646b23f8ee
snowari <70631889+snowari@users.noreply.github.com>
2025-03-19 22:46:57 +0900

    Title: 격자로 사각형 만들기, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 250319/격자로 사각형 만들기/print-grid-in-rectangle.js
+++ 250319/격자로 사각형 만들기/print-grid-in-rectangle.js
@@ -1,19 +1,15 @@
 const fs = require(('fs'))
-const n = fs.readFileSync(0).toString().trim().split(' ').map(Number)
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number)
+const n = input[0]
 
-let answer = Array(5).fill(0).map(()=> Array(5).fill(0))
+let answer = Array(n).fill(0).map(()=> Array(n).fill(0))
 for(let i=0; i<n ; i++){
     for(let j=0; j<n; j++){
-        if(i==0 || j==0){
-            answer[i][j] =1
+        if(i===0 || j===0){
+            answer[i][j] = 1
         }else{
             answer[i][j] = answer[i-1][j] + answer[i][j-1] + answer[i-1][j-1]
         }
-
-        
-
-    
-
     }
 }
 
