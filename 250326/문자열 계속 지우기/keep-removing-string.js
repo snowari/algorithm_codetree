1f3da170d3cb693490f58ae81a2a90aebed75fd3
snowari <70631889+snowari@users.noreply.github.com>
2025-03-26 21:32:46 +0900

    Title: 문자열 계속 지우기, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

--- 250326/문자열 계속 지우기/keep-removing-string.js
+++ 250326/문자열 계속 지우기/keep-removing-string.js
@@ -4,16 +4,16 @@ let input = fs.readFileSync(0).toString().trim().split('\n')
 let a = input[0]
 let b = input[1]
 
-for(let i=0; i<a.length ; i++){
-    let str = a.slice(i,i+b.length)
-  // console.log("str : ",str)
-    if(str === b){ //ba랑 비교
-        //console.log("str : b : ", str , b)
-        a = a.slice(0,i) + a.slice(i+b.length)
-        //console.log("a : ", a)
-        i=0
 
+let i=0
+while(i<=a.length - b.length){
+    let str = a.slice(i,i+b.length)
+    if(str === b){ 
+        a = a.slice(0,i) + a.slice(i+b.length)
+       i=0
+    }else{
+        i++
     }
 }
  
-    console.log(a)
+console.log(a)
