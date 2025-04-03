651fafb4d6855b4391fa271cb71bf097ff22b714
snowari <70631889+snowari@users.noreply.github.com>
2025-04-03 22:58:30 +0900

    Title: 함수를 이용한 윤년 판별, Time: 175ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250403/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const y = Number(input[0]);
+// Please Write your code here.
+
+
+function isLeapYear(num){
+    if(num%4===0){
+        if(num%100===0 && num%400!==0){
+            return false
+        }
+        return true
+    }
+}
+
+console.log(isLeapYear(y))
