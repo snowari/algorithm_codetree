ab7f1e883f8e684ca8470e5c6f89d787cb59a4bb
snowari <70631889+snowari@users.noreply.github.com>
2025-04-08 20:11:32 +0900

    Title: 2개 이상의 알파벳, Time: 176ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250408/2개 이상의 알파벳/more-than-one-alphabet.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const A = input[0];
+// Please Write your code here.
+
+function solution(str){
+    let cnt =0
+    for(let i=0; i<str.length-1; i = i+2){
+        if(str[i] !== str[i+1])
+            cnt++
+    }
+
+    if(cnt>=2){
+        return "Yes"
+    }else{
+        return "No"
+    }
+}
+
+console.log(solution(A))
