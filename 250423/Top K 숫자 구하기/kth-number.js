c955f3a7657750828f13ffa3a26cbf5eb4ec4111
snowari <70631889+snowari@users.noreply.github.com>
2025-04-23 21:51:32 +0900

    Title: Top K 숫자 구하기, Time: 186ms, Memory: 15MB, Status: Passed - Codetree

--- 250423/Top K 숫자 구하기/kth-number.js
+++ 250423/Top K 숫자 구하기/kth-number.js
@@ -4,5 +4,5 @@ const [n, k] = input[0].split(' ').map(Number);
 const nums = input[1].trim().split(' ').map(Number);
 
 // Please Write your code here.
-const sortNums = nums.sort()
+const sortNums = nums.sort((a,b)=> a-b)
 console.log(sortNums[k-1])
