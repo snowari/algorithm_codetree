c2bfccd2dbaee96f497a623a882ec2292b66af30
snowari <70631889+snowari@users.noreply.github.com>
2025-04-26 02:08:49 +0900

    Title: 비오는 날, Time: 192ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250426/비오는 날/rainy-day.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const forecasts = input.slice(1, n + 1).map(line => line.split(' '));
+
+// Please Write your code here.
+
+
+class Weather {
+    constructor(date, day, weather){
+        this.date = date
+        this.day = day
+        this.weather = weather
+    }
+}
+
+let weathers=Array(n)
+
+forecasts.forEach((arr,idx)=>{
+   let [date, day, weather]  =  [arr[0],  arr[1], arr[2]] 
+    weathers[idx] = new Weather(date,day,weather)
+})
+
+
+for(elem of weathers){
+    if(elem.weather === 'Rain'){
+        console.log(`${elem.date} ${elem.day} ${elem.weather}`)
+        break;
+    }
+}
+
