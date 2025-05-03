31d71c4b4a61e65c2c120afb83adb674ec4f67d0
snowari <70631889+snowari@users.noreply.github.com>
2025-05-03 18:36:47 +0900

    Title: 요일 맞추기, Time: 170ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250503/요일 맞추기/guess-day-of-week.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+// Please Write your code here.
+
+const day = ["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"]
+const days = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+let cnt=0
+let daySum1
+let daySum2
+
+for(let i=0; i<m1-1 ; i++){
+    cnt += days[i]
+  
+}
+cnt += d1
+daySum1 = cnt
+cnt=0
+
+for(let i=0; i<m2-1 ; i++){
+    cnt += days[i]
+   
+}
+cnt += d2
+daySum2 = cnt
+
+let answer
+
+if (daySum1>daySum2){
+    answer = (daySum1-daySum2)%7
+   
+    answer=day[-answer+1]
+    
+}else{
+    answer = (daySum2-daySum1)%7
+    answer=day[answer+1]
+}
+
+console.log(answer)
+
+
+
+
+
