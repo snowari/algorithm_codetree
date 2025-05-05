e4bd2ac4a72ffc2775717a33a1f45cb1b6e6ba0c
snowari <70631889+snowari@users.noreply.github.com>
2025-05-06 00:02:19 +0900

    Title: 그 요일은, Time: 183ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250506/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+const A = input[1];
+
+const day = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+const days = [31,29,31,30,31,30,31,31,30,31,30,31]
+// Please Write your code here.
+
+let daysum1=0
+for(let i=m1-2; i>=0 ; i--){
+    daysum1+=days[i]
+}
+daysum1+=d1
+let daysum2=0
+for(let i=m2-2; i>=0 ; i--){
+    daysum2+=days[i]
+}
+daysum2+=d2
+//console.log(daysum1, daysum2)
+
+let tmp = daysum2-daysum1
+let answer = Math.floor(tmp/7)
+tmp = tmp%7
+let i
+day.map((n,idx)=> {
+    if(n===A){
+        i= idx
+    }
+})
+//console.log(i, tmp)
+if(i<=tmp){
+    answer++
+}
+
+
+
+console.log(answer)
+
+
+
