7d8bb23eb565709717de66ebe2ed412d3d420a01
snowari <70631889+snowari@users.noreply.github.com>
2025-05-08 00:23:43 +0900

    Title: 최대로 겹치는 구간, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250508/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250508/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -3,17 +3,19 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 
-segments.sort(key=lambda x:x[1],reverse=True)
-#print(segments)
+points = []
+for a,b in segments:
+    points.append((a,1))
+    points.append((b,-1))
 
+points.sort()
+cnt = 0
 answer = 0
-for i in range(len(segments)-1):
-    flag_b =segments[i][1]
-    next_a =segments[i+1][0]
-    if flag_b > next_a :
-        answer += 1
-    
-answer += 1
-
+for _, end in points:
+    cnt += end
+    answer = max(answer,cnt)
 
 print(answer)
+
+
+
