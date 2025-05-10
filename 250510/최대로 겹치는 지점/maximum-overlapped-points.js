15281e70db17ad99487f48887899d5f954b84a6f
snowari <70631889+snowari@users.noreply.github.com>
2025-05-10 23:48:09 +0900

    Title: 최대로 겹치는 지점, Time: 179ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250510/최대로 겹치는 지점/maximum-overlapped-points.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const segments = [];
+for (let i = 1; i <= n; i++) {
+  segments.push(input[i].split(' ').map(Number));
+}
+const answer=Array(100).fill(0)
+//console.log(answer)
+
+for(let seg of segments){
+    let start = seg[0]
+    let end = seg[1]
+    for(let i =start; i<end+1 ; i++){
+        answer[i] += 1
+    }
+}
+
+console.log(Math.max(...answer))
+
+
+
+
+
+//console.log(segments)
+
+
+
+// Please Write your code here.
