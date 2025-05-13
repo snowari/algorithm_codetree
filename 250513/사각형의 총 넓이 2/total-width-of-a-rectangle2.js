a38148eed6250760f17506395802015b3db7c8b9
snowari <70631889+snowari@users.noreply.github.com>
2025-05-13 23:56:21 +0900

    Title: 사각형의 총 넓이 2, Time: 158ms, Memory: 14MB, Status: Runtime Error - Codetree

---
+++ 250513/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const rects = input.slice(1, n + 1).map(line => line.split(" ").map(Number));
+
+const board = Array.from({ length: 100 }, () => Array(100).fill(0));
+
+for (const [x1, y1, x2, y2] of rects) {
+    for (let y = y1; y < y2; y++) {
+        for (let x = x1; x < x2; x++) {
+            board[y][x] = 1; 
+        }
+    }
+}
+
+
+let area = 0;
+for (let row of board) {
+    area += row.reduce((acc, val) => acc + val, 0);
+}
+
+console.log(area);
