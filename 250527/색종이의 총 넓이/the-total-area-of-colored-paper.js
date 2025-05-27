6d38d8a49f3291b17aa3dfa764be499a501dc157
snowari <70631889+snowari@users.noreply.github.com>
2025-05-27 23:37:21 +0900

    Title: 색종이의 총 넓이, Time: 203ms, Memory: 15MB, Status: Runtime Error - Codetree

---
+++ 250527/색종이의 총 넓이/the-total-area-of-colored-paper.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const rects = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+let matrix = Array.from(Array(201), ()=> Array(201).fill(0))
+
+rects.forEach((rect)=> {
+    const x = rect[0]
+    const y = rect[1]
+    for(let i=x ; i<x+8; i++){
+        for(let j=y; j<y+8; j++ ){
+            matrix[i][j] = 1
+        }
+    }
+})
+
+let cnt =0
+matrix.forEach((row)=>{
+    row.forEach(n=>{
+        if (n===1){
+            cnt++
+        }
+    })
+})
+
+console.log(cnt)
