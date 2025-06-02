405dff64e505cbc49a1741955063fe4063a6ee66
snowari <70631889+snowari@users.noreply.github.com>
2025-06-03 00:42:00 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 346ms, Memory: 62MB, Status: Passed - Codetree

--- 250603/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 250603/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -9,15 +9,15 @@ const matrix  = new Array(2001).fill().map(()=> new Array(2001).fill(0))
 
 
 let [x1, y1, x2, y2] = [rect1[0]+1000, rect1[1]+1000, rect1[2]+1000, rect1[3]+1000]
-for(let i=x1 ; i<=x2; i++){
-    for(let j=y1; j<=y2; j++){
+for(let i=x1 ; i<x2; i++){
+    for(let j=y1; j<y2; j++){
         matrix[i][j] = 1
     }
 }
 
 [x1, y1, x2, y2] = [rect2[0]+1000, rect2[1]+1000, rect2[2]+1000, rect2[3]+1000]
-for(let i=x1 ; i<=x2; i++){
-    for(let j=y1; j<=y2; j++){
+for(let i=x1 ; i<x2; i++){
+    for(let j=y1; j<y2; j++){
         matrix[i][j] = 0
     }
 }
@@ -35,18 +35,11 @@ matrix.forEach((row, i)=> {
     })
 })
 
-let answer
-const getAreaSize = (x1, y1, x2, y2) => {
-    answer = (x2-x1)*(y2-y1) 
-    return answer
-}
+let answer=0
 
-if(minx===Infinity || maxx=== Infinity) {
-    answer = 0
-}else{
-    getAreaSize(minx, miny, maxx, maxy)
+if (minx !== Infinity) {
+    answer = (maxx - minx + 1) * (maxy - miny + 1);
 }
-
 
 console.log(answer)
 
