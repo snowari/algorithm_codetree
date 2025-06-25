99366b6d64e58f28fe0a15540b2b80aed08c7bc4
snowari <70631889+snowari@users.noreply.github.com>
2025-06-25 23:40:32 +0900

    Title: 연속되는 수 3, Time: 183ms, Memory: 15MB, Status: Passed - Codetree

--- 250625/연속되는 수 3/continuous-number3.js
+++ 250625/연속되는 수 3/continuous-number3.js
@@ -7,31 +7,16 @@ const arr = input.slice(1, 1 + n).map(Number);
 // Please Write your code here.
 
 let cnt =1 
-let answer = 0
 let max = 1
-arr[0] > 0 ? answer = 1 : answer = -1
-for (let i=1; i<arr.length ; i++){
-    if(answer>0){
-        if(arr[i]>0){
-            cnt ++
-        }else{
-            answer = -1
-            if(cnt >= max){
-                max = cnt;
-            }
-            cnt = 1;
-        }
+for (let i=1; i<n ; i++){
+    if( (arr[i] > 0 && arr[i-1] > 0) || (arr[i] < 0 && arr[i-1] <0)){
+        cnt ++;
     }else{
-        if(arr[i]<0){
-            cnt ++;
-        }else{
-            answer = 1
-            if(cnt >= max){
-                max = cnt;
-            }
-            cnt = 1;
-        }
+        if(cnt>max) max= cnt
+        cnt=1
     }
 }
+
+if(cnt> max) max= cnt
 
 console.log(max)
