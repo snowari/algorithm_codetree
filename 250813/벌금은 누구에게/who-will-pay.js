a99ab4bbd2071a408c5c345997059d804942e08f
snowari <70631889+snowari@users.noreply.github.com>
2025-08-13 02:16:08 +0900

    Title: 벌금은 누구에게, Time: 200ms, Memory: 15MB, Status: Passed - Codetree

--- 250813/벌금은 누구에게/who-will-pay.js
+++ 250813/벌금은 누구에게/who-will-pay.js
@@ -8,13 +8,18 @@ const penalizedPersons = input.slice(1, m + 1).map(Number);
 let result = Array(n+1).fill(0)
 //console.log(result)
 
+let check = -1
+
 for(let i=0; i<penalizedPersons.length; i++){
     const person = penalizedPersons[i]
     result[person]++;
     if(result[person]>=k){
         console.log(person)
+        check = 0;
         return;
     }
         
 }
 
+
+check ==  -1 ? console.log("-1") : ""
