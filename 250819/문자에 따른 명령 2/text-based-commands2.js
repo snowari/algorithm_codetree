25a460cc0ddf8a4c2c846ddac011c99027160515
snowari <70631889+snowari@users.noreply.github.com>
2025-08-19 23:30:38 +0900

    Title: 문자에 따른 명령 2, Time: 186ms, Memory: 19MB, Status: Passed - Codetree

--- 250819/문자에 따른 명령 2/text-based-commands2.js
+++ 250819/문자에 따른 명령 2/text-based-commands2.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const commands = input[0];
 
 const dx = [1,0,-1,0]
-const dy = [0,1,0,-1]
+const dy = [0,-1,0,1]
 
 let x = 0, y=0
 let dirNum =3
