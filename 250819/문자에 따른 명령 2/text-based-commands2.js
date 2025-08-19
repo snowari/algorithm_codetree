d09fcc559a0c97e1743237d02c3db05ccfe14553
snowari <70631889+snowari@users.noreply.github.com>
2025-08-19 23:28:15 +0900

    Title: 문자에 따른 명령 2, Time: 171ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250819/문자에 따른 명령 2/text-based-commands2.js
+++ 250819/문자에 따른 명령 2/text-based-commands2.js
@@ -3,24 +3,24 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const commands = input[0];
 
 const dx = [1,0,-1,0]
-const dy = [0,-1,0,1]
-
-//console.log(commands[0])
+const dy = [0,1,0,-1]
 
 let x = 0, y=0
 let dirNum =3
-
-if(commands[0]==="R"){
-    dirNum = (dirNum+1) %4
-}else{
-    dirNum = (dirNum-1+4) %4
+ 
+for (let c of commands){
+    if(c==="R"){
+        dirNum = (dirNum+1) %4
+    }else if(c==="L"){
+        dirNum = (dirNum+3) %4
+    }else{
+        continue
+    }
+    x+=dx[dirNum]
+    y+=dy[dirNum]
 }
 
-//console.log(dirNum)
 
-let nx = x+dx[dirNum]
-let ny = y+dy[dirNum]
-
-console.log(nx,ny)
+console.log(x,y)
 
 
