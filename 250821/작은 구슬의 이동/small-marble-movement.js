f4ac85ccb12c1dcb675a325abaf847f97ff8ba10
snowari <70631889+snowari@users.noreply.github.com>
2025-08-21 23:56:59 +0900

    Title: 작은 구슬의 이동, Time: 175ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250821/작은 구슬의 이동/small-marble-movement.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, t] = input[0].split(" ").map(Number);
+let [r, c, d] = input[1].split(" ");
+r = Number(r);
+c = Number(c);
+
+// dx, dy: R, L, U, D
+const dx = [1,-1,0,0];
+const dy = [0,0,1,-1];
+
+const mapper = {
+    "R" : 0,
+    "L" : 1,
+    "U" : 2,
+    "D" : 3
+};
+
+function reverseDir(d) {
+  if (d === 0) return 1;
+  if (d === 1) return 0;
+  if (d === 2) return 3;
+  return 2;
+}
+
+let x=r, y=c;
+let dir = mapper[d]; 
+
+for (let i = 0; i < t; i++) {
+  const nx = x + dx[dir];
+  const ny = y + dy[dir];
+
+  if (nx < 1 || nx > n || ny < 1 || ny > n) {
+    dir = reverseDir(dir);
+    
+  } else {
+    x = nx;
+    y = ny;
+  }
+}
+
+console.log(x, y);
