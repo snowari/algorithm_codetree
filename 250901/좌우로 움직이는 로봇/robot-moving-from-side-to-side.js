214593068066d8830d6683188c2a2947f1e14c61
snowari <70631889+snowari@users.noreply.github.com>
2025-09-01 22:23:19 +0900

    Title: 좌우로 움직이는 로봇, Time: 73ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250901/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const movesA = input.slice(1, 1 + n);
+const movesB = input.slice(1 + n, 1 + n + m);
+
+// Please Write your code here.
+
+
+console.log(movesA, movesB)
+let a, b =0
