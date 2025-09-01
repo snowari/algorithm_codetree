670c1c9979037afd7c6a037eba9bd7bffdd7213a
snowari <70631889+snowari@users.noreply.github.com>
2025-09-02 00:00:47 +0900

    Title: 좌우로 움직이는 로봇, Time: 146ms, Memory: 40MB, Status: Wrong Answer - Codetree

---
+++ 250902/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const parseMove = (s) => {
+    const [num, ch] = s.trim().split(/\s+/); 
+    return [Number(num), ch]
+}
+
+const moves = input.slice(1,1+n+m).map(parseMove)
+const lastOf = (arr) => (arr.length ? arr[arr.length -1] : 0)
+
+let movesA = moves.slice(0,n);
+let movesB = moves.slice(n);
+
+const expandTimeline = (moves) => {
+    const pos = []
+    let cur = 0;
+    for (const [dist, dir] of moves){
+        const step = dir === "R" ? 1 : -1
+        for (let i=0; i<dist; i++){
+            cur += step
+            pos.push(cur)
+        } 
+    }
+    return pos
+}
+
+const posA = expandTimeline(movesA)
+const posB = expandTimeline(movesB)
+
+//console.log(posA, posB)
+
+const max = Math.max(posA.length, posB.length)
+let meet=0;
+let prevEqual = false
+
+for(let t=0; t< max; t++){
+    const aPos = t<posA.length ? posA[t]  : lastOf(posA)
+    const bPos = t<posB.length ? posB[t]  : lastOf(posB)
+    const equal = (aPos === bPos)
+    if(equal && !prevEqual){
+    
+    meet ++
+    //console.log(t)    
+    } 
+    prevEqual = equal
+        
+}
+
+if (posA.length !== posB.length && prevEqual) {
+    meet--;
+}
+
+console.log(meet)
