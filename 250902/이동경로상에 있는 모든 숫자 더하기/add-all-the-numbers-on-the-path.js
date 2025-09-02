93670a0095a0d8b129e951f9672289e9cbe256e4
snowari <70631889+snowari@users.noreply.github.com>
2025-09-02 17:10:41 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 79ms, Memory: 11MB, Status: Passed - Codetree

--- 250902/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.js
+++ 250902/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.js
@@ -1,11 +1,40 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, t] = input[0].split(' ').map(Number);
-const commands = input[1];
+let commands = input[1];
 const board = input.slice(2, 2 + n).map(row => row.split(' ').map(Number));
 // Please Write your code here.
 
-const dx = [0,1,-1,0], dy =[1,0,0,-1]
+const dx = [-1, 0, 1, 0];
+const dy = [ 0, 1, 0,-1];
+//북동남서
 
-console.log(commands)
-console.log(board)
+
+const mapper = { "R" : 0 , "D" : 3, "U" : 1, "L" : 2}
+
+
+//console.log(board)
+let x= Math.floor(n/2)
+let y = Math.floor(n/2)
+let answer = board[x][y]
+const inRange = (x,y) => (x>=0 && x<n && y>=0 && y<n)
+
+dirNum = 0 //북쪽 바라보고시작
+commands = commands.split('')
+commands.forEach(c =>{
+    if(c==='R'){
+        dirNum = (dirNum +1) %4
+    }else if(c==="L"){
+        dirNum = (dirNum +3) %4
+    }else{
+        let nx = x+dx[dirNum]
+        let ny = y+dy[dirNum]
+        if(inRange(nx,ny)){
+            x=nx; y=ny;
+            //console.log(board[x][y])
+            answer += board[x][y]
+        }
+    }
+})
+
+console.log(answer)
