9381ebcd232582cfa33085bc21ecd270da890433
snowari <70631889+snowari@users.noreply.github.com>
2025-09-04 21:33:57 +0900

    Title: 체크판위에서 2, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250904/체크판위에서 2/on-the-checkboard-2.js
+++ 250904/체크판위에서 2/on-the-checkboard-2.js
@@ -1,29 +1,31 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split("\n");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [R, C] = input[0].split(" ").map(Number);
-const board = input.slice(1).map(line => line.trim().split(""));
+const [R, C] = input[0].split(' ').map(Number);
+const grid = input.slice(1).map(line => line.trim().split(' '));
 
-function solve(grid) {
-  const R = grid.length;
-  const C = grid.length;                 
-  const start = grid;            
-  const end = grid[R - 1][C - 1];
+// Please Write your code here.
+function solution(grid) {
+    const start = grid[0][0]
+    const end = grid[R-1][C-1]
 
-  let cnt = 0;
-  for (let r1 = 1; r1 <= R - 2; r1++) {
-    for (let c1 = 1; c1 <= C - 2; c1++) {
-      if (start === grid[r1][c1]) continue;  // start와 같으면 불가
-      for (let r2 = r1 + 1; r2 <= R - 2; r2++) {
-        for (let c2 = c1 + 1; c2 <= C - 2; c2++) {
-          const mid1 = grid[r1][c1];
-          const mid2 = grid[r2][c2];
-          if (mid1 !== mid2 && mid2 !== end) cnt++;
+    let cnt =0
+
+    for(let r1 =1; r1<R-1; r1++){
+        for(let c1=1;c1<C-1; c1++){
+            if(grid[r1][c1] === start) continue; //색 같으면 건너뜀
+            for(let r2= r1+1; r2<R-1; r2++){
+                for(let c2= c1+1; c2<R-1; c2++){
+                    const mid1= grid[r1][c1]
+                    const mid2= grid[r2][c2]
+
+                    if(mid1 !== mid2 && mid2 !==  end)
+                        cnt++
+                }
+            }
         }
-      }
     }
-  }
-  return cnt;
+    return cnt
 }
 
-console.log(solve(board));
+console.log(solution(grid))
