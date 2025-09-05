27a83864b99d88c72b751eaefea7e04e7d4d1f7e
snowari <70631889+snowari@users.noreply.github.com>
2025-09-05 16:55:02 +0900

    Title: 특정 구간의 원소 평균값, Time: 85ms, Memory: 12MB, Status: Passed - Codetree

--- 250905/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.js
+++ 250905/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.js
@@ -4,29 +4,29 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input[1].trim().split(' ').map(Number);
 
-let answer = 0
+let answer = 0;
 
-for(let i=0; i<n; i++){
-    for(let j=i; j<n; j++){
-        let sum =0;
-        for(let k=i; k<=j ; k++){
-            sum+=arr[k]
+for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) { 
+        
+        const subArray = arr.slice(i, j + 1); 
+        let sum = 0;
+        for (const num of subArray) {
+            sum += num;
         }
-    
 
-        let itemCnt = j-i+1
-        if(sum % itemCnt === 0) {
-            const avg = sum / itemCnt
-            
+        const itemCnt = subArray.length;
+        
+        if (sum % itemCnt === 0) {
+            const avg = sum / itemCnt;
 
-            for(let k=i; k<j; k++){
-                if(avg === arr[k])
-                answer ++
+            if (subArray.includes(avg)) {
+                answer++;
             }
         }
     }
 }
 
-answer+=arr.length
+console.log(answer);
 
-console.log(answer)
+
