f846250a0ff396b59bf6b48768398c1eaf8ffb4d
snowari <70631889+snowari@users.noreply.github.com>
2025-09-13 00:21:37 +0900

    Title: 운행 되고 있는 시간, Time: 70ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250913/운행 되고 있는 시간/hours-in-service.js
+++ 250913/운행 되고 있는 시간/hours-in-service.js
@@ -6,39 +6,26 @@ const segments = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
 
-let maxTime = 0
-let removedIdx = -1
-for(let i=0; i<n ; i++){
-let timeTable = Array(1001).fill(0)
-    for(let j=0; j<n; j++){
-        if(i===j){
-            continue;
-        }
-        let [start, end] = segments[j]
-        for(let k=start ; k<=end; k++){
-            timeTable[k] ++;
-        }
-    }
-    let time = 0    
-    for(let k=0; k<timeTable.length; k++){
-        if(timeTable[k] === n-1){
-            time++;
-        }
-    }
-    if(time>= maxTime){
-        maxTime = time
-        removedIdx = i
-    }
-}
-
-let answer = 0
+let removedIndex = -1
+let minLength = Infinity
 
 for(let i=0; i<n; i++){
-    if(i===removedIdx){
+    const [start, end] = segments[i]
+    const length = end-start
+
+    if(length < minLength){
+        minLength = length
+        removedIndex = i
+    }
+}
+
+let totalTime = 0
+for (let i=0; i<n; i++){
+    if( i===removedIndex){
         continue;
     }
-    answer += segments[i][1]- segments[i][0]
-    //console.log(answer)
+    const [start, end] = segments[i]
+    totalTime += end-start
 }
-//
-console.log(answer-1)
+
+console.log(totalTime-1)
