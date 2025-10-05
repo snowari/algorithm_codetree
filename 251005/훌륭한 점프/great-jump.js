3a6bccf9d246019659d1a17334660a1cc2fe5017
snowari <70631889+snowari@users.noreply.github.com>
2025-10-05 23:57:30 +0900

    Title: 훌륭한 점프, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 251005/훌륭한 점프/great-jump.js
+++ 251005/훌륭한 점프/great-jump.js
@@ -3,14 +3,14 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number);
 
-function isPossible(minVal) {
-    if (arr[0] < minVal || arr[n - 1] < minVal) {
+function isPossible(maxVal) {
+    if (arr[0] > maxVal || arr[n - 1] > maxVal) {
         return false;
     }
 
     const availableIndices = [];
     arr.forEach((elem, i) => {
-        if (elem >= minVal) {
+        if (elem <= maxVal) {
             availableIndices.push(i);
         }
     });
@@ -21,16 +21,13 @@ function isPossible(minVal) {
             return false;
         }
     }
+    
     return true;
 }
 
-let maximin = 0;
-for (let a = 1; a <= Math.min(arr[0], arr[arr.length - 1]); a++) {
-    if (isPossible(a)) {
-        maximin = a;
-    } else {
+for (let m = Math.max(arr[0], arr[n - 1]); m <= 100; m++) {
+    if (isPossible(m)) {
+        console.log(m);
         break;
     }
 }
-
-console.log(maximin);
