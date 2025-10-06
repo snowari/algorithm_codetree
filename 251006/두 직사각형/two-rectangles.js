b084010360c0db3f3f337df2d9ee57599ec2a208
snowari <70631889+snowari@users.noreply.github.com>
2025-10-06 22:12:58 +0900

    Title: 두 직사각형, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251006/두 직사각형/two-rectangles.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [x1, y1, x2, y2] = input[0].split(' ').map(Number);
+const [a1, b1, a2, b2] = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+
+if(x2 < a1 && y2< b1 || a2< x1 && b2< y1 ){
+    console.log("nonoverlapping")
+}else{
+    console.log("overlapping")
+}
