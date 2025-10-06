67551c8e6bd6021862432ede7449edca96872cb4
snowari <70631889+snowari@users.noreply.github.com>
2025-10-06 22:32:21 +0900

    Title: 두 직사각형, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 251006/두 직사각형/two-rectangles.js
+++ 251006/두 직사각형/two-rectangles.js
@@ -6,8 +6,9 @@ const [a1, b1, a2, b2] = input[1].split(' ').map(Number);
 
 // Please Write your code here.
 
-if((x2 < a1 && y2< b1) || (a2< x1 && b2< y1) ){
-    console.log("nonoverlapping")
-}else{
-    console.log("overlapping")
+if (x2 < a1 || x1 > a2 || y2 < b1 || y1 > b2) {
+    console.log("nonoverlapping");
+} else {
+    
+    console.log("overlapping");
 }
