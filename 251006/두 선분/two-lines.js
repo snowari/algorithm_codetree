abe3f5981d94742d58780042a23dde5cdb0bbe34
snowari <70631889+snowari@users.noreply.github.com>
2025-10-06 21:46:27 +0900

    Title: 두 선분, Time: 76ms, Memory: 10MB, Status: Passed - Codetree

--- 251006/두 선분/two-lines.js
+++ 251006/두 선분/two-lines.js
@@ -3,8 +3,8 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [x1, x2, x3, x4] = input[0].split(' ').map(Number);
 // Please Write your code here.
 
- if(x3<= x2 || x1>= x3){
-    console.log("intersecting")
- }else{
+if(x2< x3 || x1 > x4 ){
     console.log("nonintersecting")
- }
+}else{
+    console.log("intersecting")
+}
