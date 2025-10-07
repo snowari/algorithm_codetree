04dd7ccd9085dc712413d58fcaf07ce21ad5f8ce
snowari <70631889+snowari@users.noreply.github.com>
2025-10-07 16:16:59 +0900

    Title: 비둘기와 전기줄, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251007/비둘기와 전기줄/pigeons-and-electric-cords.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const movements = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+const position = Array(11).fill(-1)
+
+//console.log(count)
+let count =0
+for(let i=0; i<n; i++){
+    const birdsNum = movements[i][0]
+    const birdPosition = movements[i][1]
+    if(position[birdsNum]!== -1 && position[birdsNum] !== birdPosition){ //서잇는 곳 바뀜
+        count++
+    }else{
+        position[birdsNum] = birdPosition
+    }
+}
+
+console.log(count)
