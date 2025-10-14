f0db9a55c3ca45d54d35ccb48847e7823813f629
snowari <70631889+snowari@users.noreply.github.com>
2025-10-14 16:17:39 +0900

    Title: 움직이는 블록, Time: 76ms, Memory: 11MB, Status: Runtime Error - Codetree

---
+++ 251014/움직이는 블록/moving-block.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const blocks = input.slice(1, n + 1).map(Number);
+
+const flag = arr.reduce(cur => cur+ acc)
+console.log(flag)
+while(arr[i])
+
+// Please Write your code here.
