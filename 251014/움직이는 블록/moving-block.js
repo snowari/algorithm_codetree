947d9d2c16aac918582e14ed4b79fec509fba14c
snowari <70631889+snowari@users.noreply.github.com>
2025-10-14 16:42:36 +0900

    Title: 움직이는 블록, Time: 77ms, Memory: 12MB, Status: Passed - Codetree

--- 251014/움직이는 블록/moving-block.js
+++ 251014/움직이는 블록/moving-block.js
@@ -2,10 +2,13 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const blocks = input.slice(1, n + 1).map(Number);
+let blocks = input.slice(1, n + 1).map(Number);
 
-const flag = arr.reduce(cur => cur+ acc)
-console.log(flag)
-while(arr[i])
+const avg = blocks.reduce((a,b) => a+b, 0) /n 
+let answer = 0
 
-// Please Write your code here.
+for (const n of blocks){
+    if (n>avg) answer += n-avg
+}
+
+console.log(answer)
