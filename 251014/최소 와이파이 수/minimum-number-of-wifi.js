6b4f3e70b05c84437f8498e4de8777a4f2ef854c
snowari <70631889+snowari@users.noreply.github.com>
2025-10-14 17:00:54 +0900

    Title: 최소 와이파이 수, Time: 70ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251014/최소 와이파이 수/minimum-number-of-wifi.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const arr = input[1].trim().split(' ').map(Number);
+
+// Please Write your code here.
+const range =  m*2 //좌우 와이파이 거리
+let cnt =0
+let wifi = 0
+for(let i=0 ; i< n ; i++){
+     if(arr[i]>0){
+        cnt ++
+     }
+     if(cnt==range){
+        wifi++
+        cnt =0
+     }
+}
+console.log(wifi)
